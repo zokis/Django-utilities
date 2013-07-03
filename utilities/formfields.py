2f44edee0dbf2f267b058aa403c36b316712e6d1
Marcelo Tambalo <marcelo.zokis@gmail.com>
2013-07-03 17:57:09 -0300

    PEP 8

--- utilities/formfields.py
+++ utilities/formfields.py
@@ -6,12 +6,14 @@ import re
 
 
 class BRPhoneNumberField(Field):
+
     def clean(self, value):
         super(BRPhoneNumberField, self).clean(value)
         if value in EMPTY_VALUES:
             return u''
         value = re.sub('(\(|\)|\s+)', '', smart_unicode(value))
-        m = re.compile(r'^(\d{2})[-\.]?((\d{5})|(\d{4}))[-\.]?(\d{4})$').search(value)
+        m = re.compile(
+            r'^(\d{2})[-\.]?((\d{5})|(\d{4}))[-\.]?(\d{4})$').search(value)
         if m:
             gs = m.groups()
             return u'%s-%s-%s' % (gs[0], gs[1], gs[4])
