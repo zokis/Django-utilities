ea81687ea59ae28eb50082cf5a14235758da5efb
Marcelo Tambalo <marcelo.zokis@gmail.com>
2013-07-03 21:06:15 -0300

    adiciona templatetags para json

---
+++ utilities/templatetags/json.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+from django import template
+
+from utilies.utils import instance_to_json
+
+import json
+
+register = template.Library()
+
+
+def instance_to_json_filter(instance, *args):
+    return instance_to_json(instance, exclude=args)
+
+register.filter('instance_to_json', instance_to_json_filter)
+register.filter('json_dumps', json.dumps)
